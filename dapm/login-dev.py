1bc72058d4789defba55c295bdc514cba39141d8
hexsix <hexsixology@gmail.com>
2018-09-02 11:07:27 +0800

    理解登陆过程

--- dapm/login-dev.py
+++ dapm/login-dev.py
@@ -7,7 +7,7 @@ import urllib.request, urllib, http.cookiejar, re
     参数说明parms:
     username:用户名(必填),
     password :密码(必填),
-    domain:网站域名，注意格式必须是：http://www.xxx.xx/(必填),
+    domain:网站域名，注意格式必须是：http://www.xxx.xx/ (必填),
     answer:问题答案,
     questionid:问题ID,
     referer:跳转地址
@@ -15,6 +15,12 @@ import urllib.request, urllib, http.cookiejar, re
 """
 
 def login_dz(**parms):
+    """
+    函数接受一个字典 dic
+    必须含有三个关键字：domain，username 和 password
+    返回登录成功与否 True/False
+    """
+
     # 初始化
     parms_key = ['domain','answer','password','questionid','referer','username']
     arg = {}
@@ -24,17 +30,24 @@ def login_dz(**parms):
         else:
             arg[key] = ''
 
-    # cookie设置
-    cookieFile = './kan_cookies.dat'
-    cookie = http.cookiejar.LWPCookieJar()
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+    # COOOOOOOOOOOOOOOOOOOOOOOOKIE
+    # http.cookiejar 总共有四个主要对象，它们的关系是
+    # CookieJar ——> 派生 ——> FileCookieJar ——> 派生 ——> MozillaCookieJar 和 LWPCookieJar
+    # 要打开 URL，传统的 urllib.request.urlopen() 函数不支持 cookie，使用 urllib.request.build_opener() 函数来创建自己的自定义 Opener 对象
+    # 创建完成后 使用 Opener().open() 打开 URL，或者使用 urllib.request.install_opener(Opener()) 安装此 opener，并通过 urllib.request.urlopen() 打开 URL
+    cookie_filename = './' + arg['username'] + '.cookie'
+    cj = http.cookiejar.LWPCookieJar(cookie_filename)
+    handler = urllib.request.HTTPCookieProcessor(cj)
+    opener = urllib.request.build_opener(handler)
 
-    # 获取formhash
-    pre_login = arg['domain'] + 'member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login'
-    c = opener.open(pre_login).read()
-    cookie.save(cookieFile)
+    # 获取 formhash
+    # formhash 的存在是为了避免多次提交，在 discuz 上 POST 数据时，必须要有 formhash
+    # formhash 在访问网站时(即使不登录)在网页源码中写明，用正则表达式提取它
+    URL_login = arg['domain'] + 'member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login'
+    response = opener.open(URL_login)
+    source_code = response.read().decode('utf-8')
     patt = re.compile(r'.*?name="formhash".*?value="(.*?)".*?')
-    formhash = patt.search(c.decode('utf-8'))
+    formhash = patt.search(source_code)
     if not formhash:
         raise Exception('GET formhash Fail!')
     formhash = formhash.group(1)
@@ -48,26 +61,22 @@ def login_dz(**parms):
         'referer':arg['domain'] if arg['referer']=='' else arg['referer'],
         'username':arg['username'],
     }
-
     postdata = urllib.parse.urlencode(postdata).encode(encoding='utf-8')
     req = urllib.request.Request(
-        url= arg['domain']+'member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash=LCaB3&inajax=1',
-        data=postdata
+        url = arg['domain'] + 'member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash=Lb833&inajax=1',
+        data = postdata
     )
-    c = opener.open(req).read(300).decode('utf-8')
+    response = opener.open(req)
+    source_code = response.read(300).decode('utf-8')
     flag = '登陆失败 %s'%arg['username']
-    if 'succeedhandle_login' in c:
+    if 'succeedhandle_login' in source_code:
         flag = True
     return flag
 
-#使用例子：基本参数登陆
-user = 'administrator'
-pwd = 'r7))thl7^6QD'
-dom = 'http://10.51.120.224/upload/'
-
-try:
-    flag = login_dz(username=user,password=pwd,domain=dom)
-    print(flag)
-except Exception as e:
-    print('Error:',e)
+if __name__ == '__main__':
+    try:
+        flag = login_dz(username='administrator', password='r7))thl7^6QD', domain='http://10.51.120.224/upload/')
+        print(flag)
+    except Exception as e:
+        print('Error:',e)
 
